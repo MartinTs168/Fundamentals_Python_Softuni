8ee36a398ad431cee50056ea30b87a126e817356
Martin Tsenev <145794822+MartinTs168@users.noreply.github.com>
2023-09-27 19:23:00 +0300

    changes to strange zoo

--- List_basics/strange_zoo.py
+++ List_basics/strange_zoo.py
@@ -1,6 +1,6 @@
-tail = input()
-body = input()
-head = input()
-animal = [tail, body, head]
+animal = []
+for _ in range(3):
+    animal.append(input())
+
 animal[0], animal[2] = animal[2], animal[0]
 print(animal)
