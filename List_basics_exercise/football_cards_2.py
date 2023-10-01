95a388130fd239a54e7939aeee08b91a92055bd9
Martin Tsenev <145794822+MartinTs168@users.noreply.github.com>
2023-10-01 16:33:06 +0300

    Added a function to check if one of the teams can't continue playing.

--- List_basics_exercise/football_cards_2.py
+++ List_basics_exercise/football_cards_2.py
@@ -1,3 +1,8 @@
+def game_cannot_continue(team_A, team_B):
+    if len(team_A) < 7 or len(team_B) < 7:
+        return True
+
+
 team_A = ["A-1", "A-2", "A-3", "A-4", "A-5", "A-6", "A-7", "A-8", "A-9", "A-10", "A-11"]
 team_B = ["B-1", "B-2", "B-3", "B-4", "B-5", "B-6", "B-7", "B-8", "B-9", "B-10", "B-11"]
 card = input().split(" ")
@@ -7,12 +12,18 @@ for player in card:
         continue
     if player in team_A:
         team_A.remove(player)
+        if game_cannot_continue(team_A, team_B):
+            terminated_game = True
+            break
     elif player in team_B:
         team_B.remove(player)
-    if len(team_A) < 7 or len(team_B) < 7:
-        terminated_game = True
-        break
+        if game_cannot_continue(team_A, team_B):
+            terminated_game = True
+            break
+    # if len(team_A) < 7 or len(team_B) < 7:
+    #     terminated_game = True
+    #     break
 print(f"Team A - {len(team_A)}; Team B - {len(team_B)}")
 if terminated_game:
     print("Game was terminated")
-#this is more optimal put a function here for more efficency
+# this is more optimal put a function here for more efficency
