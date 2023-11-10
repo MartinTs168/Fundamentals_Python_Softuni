dbc44ad0f8b255d2b356f7ffa3877cafab83a9ff
Martin Tsenev <145794822+MartinTs168@users.noreply.github.com>
2023-11-10 17:45:16 +0200

    Added solutions to 5 new problems.

---
+++ text_procesing/digits_leters_numbers.py
@@ -0,0 +1,14 @@
+text = input()
+letters = ''
+digits = ''
+others = ''
+for ch in text:
+    if ch.isdigit():
+        digits += ch
+    elif ch.isalpha():
+        letters += ch
+    else:
+        others += ch
+print(digits)
+print(letters)
+print(others)
